e8451f24e65446b85acd305c69e09c5af31473db
adel-lamloum <adel.lamloum@gmail.com>
2025-01-12 15:07:27 +0200

    hyprland file manager

--- my-scripts/fonts-adel.sh
+++ my-scripts/fonts-adel.sh
@@ -1,19 +1,39 @@
-# Fonts
-sudo pacman -S --noconfirm --needed ttf-bitstream-vera
-sudo pacman -S --noconfirm --needed ttf-dejavu
-sudo pacman -S --noconfirm --needed ttf-droid
-sudo pacman -S --noconfirm --needed ttf-hack
-sudo pacman -S --noconfirm --needed ttf-inconsolata
-sudo pacman -S --noconfirm --needed ttf-liberation
-sudo pacman -S --noconfirm --needed ttf-roboto
-sudo pacman -S --noconfirm --needed ttf-roboto-mono
-sudo pacman -S --noconfirm --needed ttf-ubuntu-font-family
-sudo pacman -S --noconfirm --needed ttf-cascadia-code-nerd  
-sudo pacman -S --noconfirm --needed ttf-font-awesome
-sudo pacman -S --noconfirm --needed awesome-terminal-fonts
-sudo pacman -S --noconfirm --needed ttf-jetbrains-mono-nerd
-sudo pacman -S --noconfirm --needed ttf-jetbrains-mono
-sudo pacman -S --noconfirm --needed otf-hermit
-sudo pacman -S --noconfirm --needed ttf-firacode-nerd 
+
+#!/bin/bash
+
+###################################################################################################################################
+#                                                                                                                                 #
+# Author: ADEL LAMLOUM                                                                                                            #
+# Title : Install Adel Default Fonts                                                                                              #
+#                                                                                                                                 #
+###################################################################################################################################
+
+# Update the system
+sudo pacman -Syu --noconfirm
+
+# List of fonts to install
+FONTS=(
+    ttf-bitstream-vera
+    ttf-dejavu
+    ttf-droid
+    ttf-hack
+    ttf-inconsolata
+    ttf-liberation
+    ttf-roboto
+    ttf-roboto-mono
+    ttf-ubuntu-font-family
+    ttf-cascadia-code-nerd
+    ttf-font-awesome
+    awesome-terminal-fonts
+    ttf-jetbrains-mono-nerd
+    ttf-jetbrains-mono
+    otf-hermit
+    ttf-firacode-nerd
+)
+
+# Install all fonts in one command
+sudo pacman -S --noconfirm --needed "${FONTS[@]}"
+
+echo "Font installation complete!"
 
 
--- my-scripts/hyprland_install_apps_adel.sh
+++ my-scripts/hyprland_install_apps_adel.sh
@@ -3,59 +3,33 @@
 ###################################################################################################################################
 #                                                                                                                                 #
 # Author: ADEL LAMLOUM                                                                                                            #
-# Title : Install Adel Default Hyprland Apps                                                                                          #
+# Title : Install Adel Default Hyprland Apps                                                                                      #
 #                                                                                                                                 #
 ###################################################################################################################################
 
-sudo pacman -S --noconfirm --needed hyprland hyprpaper hyprpicker hyprlock hyprshot waybar xdg-desktop-portal-hyprland hyprcursor kitty thunar network-manager-applet
-sudo pacman -S --nconfirm --needed jq fd # for setting hyprpaper script
-sudo pacman -S --nconfirm --needed swaync # for notofication
-sudo pacman -S --nconfirm --needed scrot # minimal screenshot
-sudo pacman -S --nconfirm --needed nwg-look GTK3 settings editor 
-sudo pacman -S --nconfirm --needed yazzi # terminal file manager
-sudo pacman -S --nconfirm --needed  meld # compare files
-sudo pacman -S --nconfirm --needed  gedit # gui file editing
-sudo pacman -S --nconfirm --needed  
-sudo pacman -S --noconfirm --needed calibre
-sudo pacman -S --noconfirm --needed ddgr # search from terminal duck duck go engine 
-sudo pacman -S --noconfirm --needed fd
-sudo pacman -S --noconfirm --needed font-manager
-sudo pacman -S --noconfirm --needed fribidi # arabic support
-sudo pacman -S --noconfirm --needed ffmpeg
-sudo pacman -S --noconfirm --needed galculator
-sudo pacman -S --noconfirm --needed gimp
-sudo pacman -S --noconfirm --needed git
-sudo pacman -S --noconfirm --needed gnome-disk-utility
-sudo pacman -S --noconfirm --needed handbrake
-sudo pacman -S --noconfirm --needed harfbuzz # arabic support
-sudo pacman -S --noconfirm --needed imagemagick
-sudo pacman -S --noconfirm --needed krita
-sudo pacman -S --noconfirm --needed kdenlive
-sudo pacman -S --noconfirm --needed lolcat
-sudo pacman -S --noconfirm --needed libreoffice-fresh
-sudo pacman -S --noconfirm --needed neofetch
-sudo pacman -S --noconfirm --needed nodejs
-sudo pacman -S --noconfirm --needed obs-studio
-sudo pacman -S --noconfirm --needed paru
-sudo pacman -S --noconfirm --needed pdfarranger
-sudo pacman -S --noconfirm --needed qutebrowser
-sudo pacman -S --noconfirm --needed qbittorrent
-sudo pacman -S --noconfirm --needed ripgrep # search tool
-sudo pacman -S --noconfirm --needed rnote
-sudo pacman -S --noconfirm --needed syncthing
-sudo pacman -S --noconfirm --needed thunderbird
-#sudo pacman -S --noconfirm --needed tumbler #to creat thumbnail for thunar
-sudo pacman -S --noconfirm --needed telegram-desktop
-sudo pacman -S --noconfirm --needed visual-studio-code-bin
-sudo pacman -S --noconfirm --needed vlc
-sudo pacman -S --noconfirm --needed video-trimmer
-sudo pacman -S --noconfirm --needed wget
-sudo pacman -S --noconfirm --needed yt-dlp 
-sudo pacman -S --noconfirm --needed zoom
-sudo pacman -S --noconfirm --needed zsh
-sudo pacman -S --noconfirm --needed zathura
-sudo pacman -S --noconfirm --needed zathura-pdf-mupdf
-sudo pacman -S --noconfirm --needed zathura-ps
-sudo pacman -S --noconfirm --needed zathura-djvu
-sudo pacman 
+# Update the system
+sudo pacman -Syu --noconfirm
 
+# Install Hyprland and related tools
+sudo pacman -S --noconfirm --needed hyprland hyprpaper hyprpicker hyprlock hyprshot waybar xdg-desktop-portal-hyprland hyprcursor
+
+# Install terminal and file management tools
+sudo pacman -S --noconfirm --needed kitty dolphin yazi meld gedit
+
+# Install system utilities
+sudo pacman -S --noconfirm --needed network-manager-applet swaync scrot nwg-look font-manager gnome-disk-utility paru
+
+# Install multimedia and graphics tools
+sudo pacman -S --noconfirm --needed calibre gimp krita kdenlive imagemagick ffmpeg vlc obs-studio handbrake video-trimmer
+
+# Install development tools
+sudo pacman -S --noconfirm --needed git nodejs ripgrep fd
+paru -S --noconfirm --needed visual-studio-code-bin
+
+# Install productivity and office tools
+sudo pacman -S --noconfirm --needed libreoffice-fresh pdfarranger rnote thunderbird telegram-desktop zoom
+
+# Install miscellaneous tools
+sudo pacman -S --noconfirm --needed neofetch lolcat yt-dlp wget zsh zathura zathura-pdf-mupdf zathura-ps zathura-djvu
+
+echo "Installation complete!"
